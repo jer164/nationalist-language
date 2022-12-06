256b619498933dc0bfe61dcaca6936bea492905c
jer164 <62763243+jer164@users.noreply.github.com>
2022-12-05 21:01:01 -0500

    added data

---
+++ README.md
@@ -0,0 +1,26 @@
+# Nationalist Language
+Data and setup script from my project on nationalist language. 
+
+
+## Uses
+
+Currently, the uses of these data are a bit limited. I am still in the process of rebuilding my original corpora due to a data loss during my move from Cleveland to Columbus. However, what remains are the frequency tables (filtered for stopwords) for each party that I analyzed. 
+
+## Included Functionality
+
+In the package is setup.R, which performs the necessary transformations and creates a plotting function. These DataFrames are frequency tables of the tokens contained in the corpora I constructed for each party. These represent a minimal, albet still demonstrative representation of the topical focuses for each party entering 2020. 
+
+I have also packaged **plot_counts()**, which leverages [ggpubr()](https://rpkgs.datanovia.com/ggpubr/) to generate plots for each frequency table. 
+
+### setup.R
+
+Running this file provides you with mutable dataframes of each frequency table.
+
+### plot_counts()
+
+Takes five arguments:
+1. **nation**: Frequency table to be plotted. Also supplies figure with [party_name].
+2. **viz**: Choice of color for the figure (defaults to Navy)
+3. **n_tokens**: Number of tokens to be plotted (defaults to 10)
+4. **figure_no**: Number to be added to figure title; determines output name of [save].
+5. **save**: Whether or not to render the plot as a .png (defaults to TRUE).
---
+++ script.R
@@ -0,0 +1,73 @@
+require(ggsci)
+require(ggpubr)
+require(glue)
+require(textcat)
+require(tidyverse)
+require(stargazer)
+
+freq_rn <- read_csv("word_freq_rn.csv", col_types = cols(...1 = col_skip())) %>% 
+  mutate(word = as.factor(word)) %>% 
+  mutate(word = fct_reorder(word, word_count, .desc = TRUE)) %>% 
+  mutate(party = as.factor('National Rally'))
+freq_afd <- read_csv("word_freq_afd.csv", col_types = cols(...1 = col_skip())) %>% 
+  mutate(word = as.factor(word)) %>% 
+  mutate(word = fct_reorder(word, word_count, .desc = TRUE)) %>% 
+  mutate(party = as.factor('Alternative for Germany'))
+freq_sd <- read_csv("word_freq_sd.csv", col_types = cols(...1 = col_skip())) %>% 
+  mutate(word = as.factor(word)) %>% 
+  mutate(word = fct_reorder(word, word_count, .desc = TRUE)) %>% 
+  mutate(party = as.factor('Sweden Democrats'))
+freq_dpp <- read_csv("word_freq_dpp.csv", col_types = cols(...1 = col_skip())) %>% 
+  mutate(word = as.factor(word)) %>% 
+  mutate(word = fct_reorder(word, word_count, .desc = TRUE)) %>% 
+  mutate(party = as.factor("Danish People's Party")) 
+freq_vb <- read_csv("word_freq_vb.csv", col_types = cols(...1 = col_skip())) %>% 
+  mutate(word = as.factor(word)) %>% 
+  mutate(word = fct_reorder(word, word_count, .desc = TRUE)) %>% 
+  mutate(party = as.factor("Vlaams Belang")) 
+
+summarized_counts <- bind_rows(freq_afd, freq_dpp, freq_rn, freq_sd, freq_vb) %>% 
+  group_by(party) %>% 
+  mutate(party = as.character.factor(party)) %>% 
+  summarise(tokens = sum(word_count),
+            unique_tokens = n_distinct(word),
+            top_token = as.character.factor(word[which.max(word_count)]))
+
+
+### Plotting Function
+
+plot_counts <- function(nation, viz = 'navy', n_tokens = 10, figure_no = 1, save = TRUE){
+
+  library(ggpubr)
+  
+  if (grepl('dpp', substitute(nation)) == TRUE){
+    party_name = "Danish People's Party"
+  } else if (grepl('rn', substitute(nation)) == TRUE){
+    party_name = "National Rally"
+  } else if (grepl('sd', substitute(nation)) == TRUE){
+    party_name = "Sweden Democrats"
+  } else if (grepl('vb', substitute(nation)) == TRUE){
+    party_name = "Vlaams Belang"
+  } else if (grepl('afd', substitute(nation)) == TRUE){
+    party_name = "AfD"
+  } else{party_name = 'Undefined Party'}
+    
+  
+  p <- nation %>% 
+    slice_head(n = n_tokens) %>%  
+    ggbarplot(x = 'word', y = 'word_count', fill = viz,
+                       xlab = 'Token', ylab = 'Frequency',
+              subtitle = glue('Frequency Plot of {party_name}'),
+              title = glue('Figure {figure_no}')) +
+    scale_x_discrete(guide = guide_axis(n.dodge=2)) +
+    font("title", size = 14, face = "bold")
+  
+  if (save == TRUE){
+    ggsave(glue('figure{figure_no}.png', dpi= 300))
+  }
+  return(p)
+}
+
+
+
+
